71bd1011582c62dd710c81a983ff514c69c96563
ycl <499475142@qq.com>
2021-12-31 13:01:53 +0800

    完善BaseMap的代码（完成）

--- rainPro-boot/rainPro-boot-base/rainPro-boot-base-tools/src/main/java/cn/soft/common/base/BaseMap.java
+++ rainPro-boot/rainPro-boot-base/rainPro-boot-base-tools/src/main/java/cn/soft/common/base/BaseMap.java
@@ -1,9 +1,14 @@
 package cn.soft.common.base;
 
 import cn.hutool.core.util.ObjectUtil;
+import org.apache.commons.beanutils.ConvertUtils;
 
+import java.math.BigDecimal;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 /**
  * 自定义map
@@ -65,5 +70,131 @@ public class BaseMap extends HashMap<String, Object> {
         } else {
             return false;
         }
+    }
+
+    /**
+     * 获取Long类型的数据
+     * @param key 键
+     * @return 返回Long类型的数据
+     */
+    public Long getLong(String key) {
+        Object v = get(key);
+        if (ObjectUtil.isNotEmpty(v)) {
+            return new Long(clone().toString());
+        } else  {
+            return  null;
+        }
+    }
+
+    /**
+     * 获取Long类型额数组数据
+     * @param key 键
+     * @return 返回Lone类型的数组数据
+     */
+    public Long[] getLongs(String key) {
+        Object v = get(key);
+        if (ObjectUtil.isNotEmpty(v)) {
+            return (Long[]) v;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取long类型的集合
+     * @param key 键
+     * @return 返回long类型的集合
+     */
+    public List<Long> getListLong(String key) {
+        List<String> list = get(key);
+        if (ObjectUtil.isNotEmpty(list)) {
+            return list.stream().map(Long::new).collect(Collectors.toList());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取long类型的数组数据
+     * @param key 键
+     * @return 返回数据
+     */
+    public Long[] getLongIds(String key) {
+        Object ids = get(key);
+        if (ObjectUtil.isNotEmpty(ids)) {
+            return (Long[]) ConvertUtils.convert(ids.toString().split(","), Long.class);
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * 获取Integer数据
+     * @param key 键
+     * @param def 默认值
+     * @return 如果取到值则返回  取不到返回默认值
+     */
+    public Integer getInt(String key, Integer def) {
+        Object o = get(key);
+        if (ObjectUtil.isNotEmpty(o)){
+            return Integer.parseInt(o.toString());
+        } else {
+            return def;
+        }
+    }
+
+    /**
+     * 获取Integer数据
+     * @param key 键
+     * @return 取到值返回值  取不到默认返回0
+     */
+    public Integer getInt(String key) {
+        Object v = get(key);
+        if (ObjectUtil.isNotEmpty(v)) {
+            return Integer.parseInt(v.toString());
+        } else {
+            return 0;
+        }
+    }
+
+    /**
+     * 获取精度数据
+     * @param key 键
+     * @return 取到则返回  取不到0
+     */
+    public BigDecimal getBigDecimal(String key) {
+        Object v = get(key);
+        if (ObjectUtil.isNotEmpty(v)) {
+            return new BigDecimal(v.toString());
+        }
+        return new BigDecimal("0");
+    }
+
+
+    /**
+     * 获取数据
+     * @param key 键
+     * @param def 默认值
+     * @param <T> 类型
+     * @return 返回取到的数据或默认值
+     */
+    @SuppressWarnings("unchecked")
+    public <T> T get(String key, T def) {
+        Object obj = super.get(key);
+        if (ObjectUtil.isEmpty(obj)) {
+            return def;
+        }
+        return (T) obj;
+    }
+
+    /**
+     * 转换为BaseMap
+     * @param obj 原始对象
+     * @return 转换后的对象
+     */
+    public static BaseMap toBaseMap(Map<String, Object> obj) {
+        BaseMap map = new BaseMap();
+        map.putAll(obj);
+        return map;
     }
 }
