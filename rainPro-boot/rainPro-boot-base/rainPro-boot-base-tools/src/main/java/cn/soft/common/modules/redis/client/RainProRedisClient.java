1432fdd54b7e9b62a305fcae799c5fa0e477fd17
ycl <499475142@qq.com>
2022-01-03 14:25:44 +0800

    添加Redis的一些配置

--- rainPro-boot/rainPro-boot-base/rainPro-boot-base-tools/src/main/java/cn/soft/common/modules/redis/client/RainProRedisClient.java
+++ rainPro-boot/rainPro-boot-base/rainPro-boot-base-tools/src/main/java/cn/soft/common/modules/redis/client/RainProRedisClient.java
@@ -1,6 +1,11 @@
 package cn.soft.common.modules.redis.client;
 
+import cn.soft.common.base.BaseMap;
+import cn.soft.common.constant.GlobalConstants;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import javax.annotation.Resource;
 
 /**
  * Redis客户端
@@ -8,4 +13,16 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class RainProRedisClient {
 
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 发送消息
+     * @param handlerName 处理器名
+     * @param params 参数
+     */
+    public void sendMessage(String handlerName, BaseMap params) {
+        params.put(GlobalConstants.HANDLER_NAME, handlerName);
+        redisTemplate.convertAndSend(GlobalConstants.REDIS_TOPIC_NAME, params);
+    }
 }
--- rainPro-boot/rainPro-boot-base/rainPro-boot-base-tools/src/main/java/cn/soft/common/modules/redis/writer/RainProRedisCacheWriter.java
+++ rainPro-boot/rainPro-boot-base/rainPro-boot-base-tools/src/main/java/cn/soft/common/modules/redis/writer/RainProRedisCacheWriter.java
@@ -1,20 +1,30 @@
 package cn.soft.common.modules.redis.writer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.PessimisticLockingFailureException;
 import org.springframework.data.redis.cache.CacheStatistics;
 import org.springframework.data.redis.cache.CacheStatisticsCollector;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStringCommands;
+import org.springframework.data.redis.core.types.Expiration;
+import org.springframework.lang.Nullable;
 import org.springframework.util.Assert;
 
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 /**
  * 参照DefaultRedisCacheWriter 重写remove方法实现通配符*删除
  */
+@Slf4j
 public class RainProRedisCacheWriter implements RedisCacheWriter {
 
     private final RedisConnectionFactory connectionFactory;
@@ -36,28 +46,97 @@ public class RainProRedisCacheWriter implements RedisCacheWriter {
         Assert.notNull(name, "Name must not be null!");
         Assert.notNull(key, "Key must not be null!");
         Assert.notNull(value, "Value must not be null!");
-
-
+        this.execute(name, (connection) -> {
+            if (shouldExpireWithin(ttl)) {
+                connection.set(key, value, Expiration.from(ttl.toMillis(), TimeUnit.MILLISECONDS), RedisStringCommands.SetOption.upsert());
+            } else {
+                connection.set(key, value);
+            }
+            return "OK";
+        });
     }
 
     @Override
     public byte[] get(String name, byte[] key) {
-        return new byte[0];
+        return this.execute(name, (connection) -> connection.get(key));
     }
 
     @Override
     public byte[] putIfAbsent(String name, byte[] key, byte[] value, Duration ttl) {
-        return new byte[0];
+        Assert.notNull(name, "Name must not be null!");
+        Assert.notNull(key, "Key must not be null!");
+        Assert.notNull(value, "Value must not be null!");
+        return (byte[])this.execute(name, (connection) -> {
+            if (this.isLockingCacheWriter()) {
+                this.doLock(name, connection);
+            }
+            Object result;
+            try {
+                boolean put;
+                if (shouldExpireWithin(ttl)) {
+                    put = connection.set(key, value, Expiration.from(ttl), RedisStringCommands.SetOption.ifAbsent());
+                } else {
+                    put = connection.setNX(key, value);
+                }
+                if (!put) {
+                    byte[] var11 = connection.get(key);
+                    return var11;
+                }
+                result = null;
+            } finally {
+                if (this.isLockingCacheWriter()) {
+                    this.doUnlock(name, connection);
+                }
+
+            }
+            return (byte[])result;
+        });
     }
 
     @Override
     public void remove(String name, byte[] key) {
-
+        Assert.notNull(name, "Name must not be null!");
+        Assert.notNull(key, "Key must not be null!");
+        String keyString = new String(key);
+        log.info("redis remove key:" + keyString);
+        if(keyString.endsWith("*")){
+            execute(name, connection -> {
+                // 获取某个前缀所拥有的所有的键，某个前缀开头，后面肯定是*
+                Set<byte[]> keys = connection.keys(key);
+                int delNum = 0;
+                for (byte[] keyByte : keys) {
+                    delNum += connection.del(keyByte);
+                }
+                return delNum;
+            });
+        }else{
+            this.execute(name, (connection) -> connection.del(new byte[][]{key}));
+        }
     }
 
     @Override
     public void clean(String name, byte[] pattern) {
+        Assert.notNull(name, "Name must not be null!");
+        Assert.notNull(pattern, "Pattern must not be null!");
+        this.execute(name, (connection) -> {
+            boolean wasLocked = false;
+            try {
+                if (this.isLockingCacheWriter()) {
+                    this.doLock(name, connection);
+                    wasLocked = true;
+                }
+                byte[][] keys = (byte[][])((Set) Optional.ofNullable(connection.keys(pattern)).orElse(Collections.emptySet())).toArray(new byte[0][]);
+                if (keys.length > 0) {
+                    connection.del(keys);
+                }
+            } finally {
+                if (wasLocked && this.isLockingCacheWriter()) {
+                    this.doUnlock(name, connection);
+                }
 
+            }
+            return "OK";
+        });
     }
 
     @Override
@@ -67,12 +146,7 @@ public class RainProRedisCacheWriter implements RedisCacheWriter {
 
     @Override
     public RedisCacheWriter withStatisticsCollector(CacheStatisticsCollector cacheStatisticsCollector) {
-        return null;
-    }
-
-    @Override
-    public CacheStatistics getCacheStatistics(String cacheName) {
-        return null;
+        return this;
     }
 
     /**
@@ -128,6 +202,16 @@ public class RainProRedisCacheWriter implements RedisCacheWriter {
     }
 
     /**
+     * 执行
+     * @param callback 回调
+     */
+    private void executeLockFree(Consumer<RedisConnection> callback) {
+        try (RedisConnection connection = this.connectionFactory.getConnection()) {
+            callback.accept(connection);
+        }
+    }
+
+    /**
      * 检测
      * @param name 名
      * @param connection 连接
@@ -146,11 +230,25 @@ public class RainProRedisCacheWriter implements RedisCacheWriter {
     }
 
     /**
+     * 是否过期的数据
+     * @param sleepTime 时间
+     * @return /
+     */
+    private static boolean shouldExpireWithin(@Nullable Duration sleepTime) {
+        return sleepTime != null && !sleepTime.isZero() && !sleepTime.isNegative();
+    }
+
+    /**
      * 创建cacheLockKey
      * @param name 名
      * @return /
      */
     private static byte[] createCacheLockKey(String name) {
         return (name + "~lock").getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public CacheStatistics getCacheStatistics(String cacheName) {
+        return null;
     }
 }
