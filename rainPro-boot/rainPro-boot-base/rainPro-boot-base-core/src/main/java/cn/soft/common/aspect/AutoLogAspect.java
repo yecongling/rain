f33fa82b51ac8bd44841db520ad8d0709b506ef8
ycl <499475142@qq.com>
2022-01-07 16:47:55 +0800

    添加一些切面保存日志所需的代码（如工具类）  目前写到记录日志的位置

--- rainPro-boot/rainPro-boot-base/rainPro-boot-base-core/src/main/java/cn/soft/common/aspect/AutoLogAspect.java
+++ rainPro-boot/rainPro-boot-base/rainPro-boot-base-core/src/main/java/cn/soft/common/aspect/AutoLogAspect.java
@@ -1,16 +1,27 @@
 package cn.soft.common.aspect;
 
+import cn.soft.common.api.vo.Result;
 import cn.soft.common.constant.CommonConstant;
+import cn.soft.common.util.oConvertUtils;
 import cn.soft.modules.base.mapper.BaseCommonMapper;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson.serializer.PropertyFilter;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.boot.context.properties.bind.BindResult;
+import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import java.lang.reflect.Method;
 
 /**
  * 系统日志  切面处理类
@@ -93,8 +104,60 @@ public class AutoLogAspect {
      * @return 返回请求的参数
      */
     public String getRequestParams(HttpServletRequest request, JoinPoint joinPoint) {
+        String heepMethod = request.getMethod();
+        StringBuilder params = new StringBuilder();
+        if ("POST".equals(heepMethod) || "PUT".equals(heepMethod) || "PATCH".equals(heepMethod)) {
+            Object[] paramsArray = joinPoint.getArgs();
+            Object[] arguments = new Object[params.length()];
+            for (int i = 0; i < paramsArray.length; i++) {
+                if (paramsArray[i] instanceof BindResult || paramsArray[i] instanceof ServletRequest || paramsArray[i] instanceof ServletResponse || paramsArray[i] instanceof MultipartFile) {
+                    continue;
+                }
+                arguments[i] = paramsArray[i];
+            }
+            // 日志数据太长的直接过滤掉
+            PropertyFilter propertyFilter = (o, name, value) -> {
+                return value == null || value.toString().length() <= 500;
+            };
+            params = new StringBuilder(JSONObject.toJSONString(arguments, propertyFilter));
+        } else {
+            MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+            Method method = signature.getMethod();
+            // 请求的方法参数值
+            Object[] args = joinPoint.getArgs();
+            // 请求的方法参数名称
+            LocalVariableTableParameterNameDiscoverer discoverer = new LocalVariableTableParameterNameDiscoverer();
+            String[] parameterNames = discoverer.getParameterNames(method);
+            if (args != null && parameterNames != null) {
+                for (int i = 0; i < args.length; i++) {
+                    params.append(" ").append(parameterNames[i]).append(": ").append(args[i]);
+                }
+            }
+        }
+        return params.toString();
+    }
 
-        return "";
+    /**
+     * online日志内容拼接
+     * @param obj 对象
+     * @param content 内容
+     * @return /
+     */
+    private String getOnlineLogContent(Object obj, String content){
+        if (obj instanceof Result){
+            Result res = (Result)obj;
+            String msg = res.getMessage();
+            String tableName = res.getOnlTable();
+            if(oConvertUtils.isNotEmpty(tableName)){
+                content+=",表名:"+tableName;
+            }
+            if(res.isSuccess()){
+                content+= ","+(oConvertUtils.isEmpty(msg)?"操作成功":msg);
+            }else{
+                content+= ","+(oConvertUtils.isEmpty(msg)?"操作失败":msg);
+            }
+        }
+        return content;
     }
 
 }
