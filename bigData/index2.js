4994cb28bb3563f4a6943ee2909e1f9d68925af6
ycl <499475142@qq.com>
2022-01-10 18:35:05 +0800

    添加临床数据中心大屏

---
+++ bigData/index2.js
@@ -0,0 +1,29 @@
+(function ($) {
+    'use strict';
+    $.index2 = {};
+    $.extend($.index2, {
+        /**
+         * 数据量占比分析饼图
+         */
+        pie: null,
+        /**
+         * 共享交换文档折线图
+         */
+        docChart: null,
+        /**
+         * EMPI地图
+         */
+        EMPIMap: null,
+        /**
+         * 初始化
+         */
+        init() {
+            // 定时刷新时间
+            // setInterval(() => {
+            //
+            // }, 1000);
+
+        }
+    });
+})(jQuery);
+
